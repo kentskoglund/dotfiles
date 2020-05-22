0ee6c351d8852f94c255b8fab865067bd000c018
Kent Skoglund <oslo6.kent@gmail.com>
2020-05-22 16:15:13 +0200

    automatic update: 22-05-2020 at 16:15:13

--- l1839/.local/bin/ccopy.sh
+++ l1839/.local/bin/ccopy.sh
@@ -11,18 +11,18 @@ rsync -ah /home/kent/.vimrc /home/kent/Nextcloud/Linux/dotfiles/l1839/ &&
 rsync -ah /home/kent/.xinitrc /home/kent/Nextcloud/Linux/dotfiles/l1839/ &&
 rsync -ah /home/kent/.zshrc /home/kent/Nextcloud/Linux/dotfiles/l1839/ &&
 rsync -ah /home/kent/.Xresources /home/kent/Nextcloud/Linux/dotfiles/l1839/ &&
-rsync -ah --delete  /home/kent/.i3/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.i3/ &&
-rsync -ah --delete  /home/kent/.local/bin/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.local/bin/ &&
-rsync -ah --delete  /home/kent/.config/i3status/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.config/i3status/ &&
-rsync -ah --delete  /home/kent/.config/i3blocks/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.config/i3blocks/ &&
-rsync -ah --delete  /home/kent/.config/alacritty/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.config/alacritty/ &&
-rsync -ah --delete  /home/kent/.config/conky/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.config/conky/ &&
-rsync -ah --delete  /home/kent/.config/dunst/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.config/dunst/ &&
-rsync -ah --delete  /home/kent/.config/nvim/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.config/nvim/ &&
-rsync -ah --delete  /home/kent/.config/terminator/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.config/terminator/ &&
-rsync -ah --delete  /home/kent/.vim/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.vim/ &&
-rsync -ah --delete  /home/kent/.config/zsh/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.config/zsh/ &&
-rsync -ah --delete  /home/kent/scripts/ /home/kent/Nextcloud/Linux/dotfiles/l1839/scripts/
+rsync -ah --delete --exclude ".git" /home/kent/.i3/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.i3/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.local/bin/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.local/bin/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.config/i3status/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.config/i3status/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.config/i3blocks/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.config/i3blocks/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.config/alacritty/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.config/alacritty/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.config/conky/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.config/conky/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.config/dunst/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.config/dunst/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.config/nvim/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.config/nvim/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.config/terminator/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.config/terminator/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.vim/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.vim/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.config/zsh/ /home/kent/Nextcloud/Linux/dotfiles/l1839/.config/zsh/ &&
+rsync -ah --delete --exclude ".git" /home/kent/scripts/ /home/kent/Nextcloud/Linux/dotfiles/l1839/scripts/
 if [ $? -eq 0 ]
 then
   echo -e "\e[32m*** Kopiering vellykket ***\e[39m"
---
+++ l1839/.local/bin/dotfiles.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# This script helps me update my dot files regularly
+# It exists in my anacrontab as:
+#
+# @daily  10      setup.backup            su -c /home/shubham1172/setup/scripts/backup.sh - shubham1172 >> /home/shubham1172/.anacronlogs 2>&1
+
+cd ${HOME}/Nextcloud/Linux/dotfiles/
+SCRIPT_PATH="/home/kent/.local/bin/ccopy.sh"
+
+timestamp() {
+  date +"%d-%m-%Y at %T"
+}
+
+# Kopier my stuff
+"$SCRIPT_PATH"
+
+# git push dotfiles
+if [[ `git status --porcelain` ]]; then
+	git pull origin master
+	git add .
+	git commit -m "automatic update: $(timestamp)"
+	git push origin master
+fi
