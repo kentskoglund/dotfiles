77896f9136212896a15bd6c1066ee06ede2f1915
Kent Skoglund <oslo6.kent@gmail.com>
2020-05-22 17:45:44 +0200

    automatic update: 22-05-2020 at 17:45:44

--- titan-man/.local/bin/ccopy.sh
+++ titan-man/.local/bin/ccopy.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-rsync -ah /home/kent/.config/aliasrc /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/ &&
-rsync -ah /home/kent/.config/mimeapps.list /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/ &&
-rsync -ah /home/kent/.config/picom.conf /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/ &&
-rsync -ah /home/kent/.gitconfig /home/kent/Nextcloud/Linux/dotfiles/titan-man/ &&
-rsync -ah /home/kent/.vimrc /home/kent/Nextcloud/Linux/dotfiles/titan-man/ &&
-rsync -ah /home/kent/.zshrc /home/kent/Nextcloud/Linux/dotfiles/titan-man/ &&
-rsync -ah /home/kent/.Xresources /home/kent/Nextcloud/Linux/dotfiles/titan-man/ &&
-rsync -ah /home/kent/.xinitrc /home/kent/Nextcloud/Linux/dotfiles/titan-man/ &&
-rsync -ah --delete --exclude ".git" /home/kent/.i3/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.i3/ &&
-rsync -ah --delete --exclude ".git" /home/kent/.local/bin/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.local/bin/ &&
-rsync -ah --delete --exclude ".git" /home/kent/.config/i3status/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/i3status/ &&
-rsync -ah --delete --exclude ".git" /home/kent/.config/i3blocks/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/i3blocks/ &&
-rsync -ah --delete --exclude ".git" /home/kent/.config/alacritty/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/alacritty/ &&
-rsync -ah --delete --exclude ".git" /home/kent/.config/conky/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/conky/ &&
-rsync -ah --delete --exclude ".git" /home/kent/.config/dunst/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/dunst/ &&
-rsync -ah --delete --exclude ".git" /home/kent/.config/nvim/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/nvim/ &&
-rsync -ah --delete --exclude ".git" /home/kent/.config/ranger/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/ranger/ &&
-rsync -ah --delete --exclude ".git" /home/kent/.config/terminator/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/terminator/ &&
-rsync -ah --delete --exclude ".git" /home/kent/.vim/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.vim/ &&
-rsync -ah --delete --exclude ".git" /home/kent/.config/zsh/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/zsh/ &&
-rsync -ah --delete --exclude ".git" /home/kent/git/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/git/ &&
-rsync -ah --delete --exclude ".git" /home/kent/scripts/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/scripts/ &&
+KOPI="/home/kent/Nextcloud/Linux/dotfiles/titan-man"
+
+rsync -ah $HOME/.config/aliasrc $KOPI/.config/ &&
+rsync -ah $HOME/.config/mimeapps.list $KOPI/.config/ &&
+rsync -ah $HOME/.config/picom.conf $KOPI/.config/ &&
+rsync -ah $HOME/.gitconfig $KOPI/ &&
+rsync -ah $HOME/.vimrc $KOPI/ &&
+rsync -ah $HOME/.zshrc $KOPI/ &&
+rsync -ah $HOME/.Xresources $KOPI/ &&
+rsync -ah $HOME/.xinitrc $KOPI/ &&
+rsync -ah --delete --exclude ".git" $HOME/.i3/ $KOPI/.i3/ &&
+rsync -ah --delete --exclude ".git" $HOME/.local/bin/ $KOPI/.local/bin/ &&
+rsync -ah --delete --exclude ".git" $HOME/.config/i3status/ $KOPI/.config/i3status/ &&
+rsync -ah --delete --exclude ".git" $HOME/.config/i3blocks/ $KOPI/.config/i3blocks/ &&
+rsync -ah --delete --exclude ".git" $HOME/.config/alacritty/ $KOPI/.config/alacritty/ &&
+rsync -ah --delete --exclude ".git" $HOME/.config/conky/ $KOPI/.config/conky/ &&
+rsync -ah --delete --exclude ".git" $HOME/.config/dunst/ $KOPI/.config/dunst/ &&
+rsync -ah --delete --exclude ".git" $HOME/.config/nvim/ $KOPI/.config/nvim/ &&
+rsync -ah --delete --exclude ".git" $HOME/.config/ranger/ $KOPI/.config/ranger/ &&
+rsync -ah --delete --exclude ".git" $HOME/.config/terminator/ $KOPI/.config/terminator/ &&
+rsync -ah --delete --exclude ".git" $HOME/.vim/ $KOPI/.vim/ &&
+rsync -ah --delete --exclude ".git" $HOME/.config/zsh/ $KOPI/.config/zsh/ &&
+rsync -ah --delete --exclude ".git" $HOME/git/ $KOPI/git/ &&
+rsync -ah --delete --exclude ".git" $HOME/scripts/ $KOPI/scripts/
 if [ $? -eq 0 ]
 then
   echo -e "\e[32m*** Kopiering vellykket ***\e[39m"
