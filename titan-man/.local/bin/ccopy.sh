0db2dd4dbbd51273e6131edbe9864f7279d74bdd
Kent Skoglund <oslo6.kent@gmail.com>
2020-05-22 15:04:34 +0200

    automatic update: 22-05-2020 at 15:04:34

--- titan-man/.local/bin/ccopy.sh
+++ titan-man/.local/bin/ccopy.sh
@@ -8,20 +8,20 @@ rsync -ah /home/kent/.vimrc /home/kent/Nextcloud/Linux/dotfiles/titan-man/ &&
 rsync -ah /home/kent/.zshrc /home/kent/Nextcloud/Linux/dotfiles/titan-man/ &&
 rsync -ah /home/kent/.Xresources /home/kent/Nextcloud/Linux/dotfiles/titan-man/ &&
 rsync -ah /home/kent/.xinitrc /home/kent/Nextcloud/Linux/dotfiles/titan-man/ &&
-rsync -ah --delete  /home/kent/.i3/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.i3/ &&
-rsync -ah --delete  /home/kent/.local/bin/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.local/bin/ &&
-rsync -ah --delete  /home/kent/.config/i3status/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/i3status/ &&
-rsync -ah --delete  /home/kent/.config/i3blocks/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/i3blocks/ &&
-rsync -ah --delete  /home/kent/.config/alacritty/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/alacritty/ &&
-rsync -ah --delete  /home/kent/.config/conky/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/conky/ &&
-rsync -ah --delete  /home/kent/.config/dunst/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/dunst/ &&
-rsync -ah --delete  /home/kent/.config/nvim/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/nvim/ &&
-rsync -ah --delete  /home/kent/.config/ranger/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/ranger/ &&
-rsync -ah --delete  /home/kent/.config/terminator/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/terminator/ &&
-rsync -ah --delete  /home/kent/.vim/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.vim/ &&
-rsync -ah --delete  /home/kent/.config/zsh/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/zsh/
-rsync -ah --delete  /home/kent/git/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/git/
-rsync -ah --delete  /home/kent/scripts/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/scripts/
+rsync -ah --delete --exclude ".git" /home/kent/.i3/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.i3/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.local/bin/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.local/bin/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.config/i3status/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/i3status/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.config/i3blocks/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/i3blocks/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.config/alacritty/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/alacritty/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.config/conky/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/conky/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.config/dunst/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/dunst/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.config/nvim/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/nvim/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.config/ranger/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/ranger/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.config/terminator/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/terminator/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.vim/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.vim/ &&
+rsync -ah --delete --exclude ".git" /home/kent/.config/zsh/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/.config/zsh/
+rsync -ah --delete --exclude ".git" /home/kent/git/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/git/
+rsync -ah --delete --exclude ".git" /home/kent/scripts/ /home/kent/Nextcloud/Linux/dotfiles/titan-man/scripts/
 if [ $? -eq 0 ]
 then
   echo -e "\e[32m*** Kopiering vellykket ***\e[39m"
---
+++ titan-man/.local/bin/dotfiles.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# This script helps me update my dot files regularly
+# It exists in my anacrontab as:
+#
+# @daily  10      setup.backup            su -c /home/shubham1172/setup/scripts/backup.sh - shubham1172 >> /home/shubham1172/.anacronlogs 2>&1
+
+cd ${HOME}/Nextcloud/Linux/dotfiles/
+
+BACKUP=${HOME}/Nextcloud/Linux/dotfiles/
+
+timestamp() {
+  date +"%d-%m-%Y at %T"
+}
+
+#grep -v '^$\|^\s*\#' $BACKUP | while read -r line; do
+#	echo "$line" | awk '{ system("rsync " $1 " " $2) }'
+#done
+
+if [[ `git status --porcelain` ]]; then
+	git pull origin master
+	git add .
+	git commit -m "automatic update: $(timestamp)"
+	git push origin master
+fi
