5a32ceb64aead9e17670c1cd2828bdccf29f5657
Kent Skoglund <oslo6.kent@gmail.com>
2020-05-26 22:13:28 +0200

    automatic update: 26-05-2020 at 22:13:28

---
+++ titan-man/.local/bin/monitor-solo.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+xrandr --output DP-0 --mode 3440x1440 --pos 0x1080 --rotate normal --primary --output DP-2 --off
---
+++ titan-man/.local/bin/monitors.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+xrandr --output DP-0 --mode 3440x1440 --pos 0x1080 --rotate normal --primary --output DP-2 --mode 1920x1080 --pos 597x0 --rotate normal
